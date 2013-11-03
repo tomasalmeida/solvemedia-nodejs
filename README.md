8257ae84ec7940ebe2a50daa56e513d47dc576d8
Tomás Dias Almeida <tomas.almeida@gmail.com>
2013-11-03 17:20:25 +0100

    First version released - Full support to colors and languages - 2 examples released - Tested

--- README.md
+++ README.md
@@ -20,6 +20,48 @@ An independent node.js module that handles calling Solve Media captchas. This mo
 
 ```
 
+### Customizations
+SolveMedia API offers some customization to your captcha (language and color)
+
+```js
+  var Solvemedia = require('solvemedia');
+
+  //Create a new instance of solvemedia, setting your three keys
+  var sm = new Solvemedia('Challenge Key','Verification Key','Authentication Hash Key');
+
+  // First: display the captcha
+  var html0 = sm.toHTML();                           // no customization
+  var html1 = sm.toHTML(sm.LANG.FR, sm.THEME.BLACK); // text in French and captcha in black
+  var html2 = sm.toHTML(sm.LANG.PT);                 // text in Portuguese (no color customized)
+  var html3 = sm.toHTML(sm.THEME.RED);               // Captcha in RED (no text customized)
+  // Display a form
+```
+
+The complete list of customization is:
+
+```js
+sm.LANG.EN
+sm.LANG.DE
+sm.LANG.FR
+sm.LANG.ES
+sm.LANG.IT
+sm.LANG.YI
+sm.LANG.JA
+sm.LANG.CA
+sm.LANG.PL
+sm.LANG.HU
+sm.LANG.SV
+sm.LANG.NO
+sm.LANG.PT
+sm.LANG.NL
+sm.LANG.TR
+
+sm.THEME.BLACK
+sm.THEME.WHITE
+sm.THEME.PURPLE
+sm.THEME.RED
+```
+
 ### Validate user response
 
 ```js
---
+++ examples/customized/app.js
@@ -0,0 +1,104 @@
+
+var express    = require('express'),
+    http       = require('http');
+    Solvemedia = require('../../lib/solvemedia');
+
+
+var app = express();
+
+//render
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('ejs', require('ejs-locals'));
+
+//Express config
+app.use(express.logger());
+app.use(express.cookieParser());
+app.use(express.methodOverride());
+app.use(express.json());
+app.use(express.urlencoded());
+app.set('port', 8080);
+
+
+app.get('/(validate)?', function(req, res) {
+
+    var solvemedia = new Solvemedia('PUBLIC_KEY','PRIVATE_KEY', 'AUTHENTICATION_KEY');
+    res.render('register', {
+        layout: false,
+        locals: {
+            name        : '',
+            captcha     : solvemedia.toHTML(), 
+            errorMessage: ''
+                
+        }
+    });
+});
+
+
+app.get('/black/fr', function(req, res) {
+
+    var solvemedia = new Solvemedia('PUBLIC_KEY','PRIVATE_KEY', 'AUTHENTICATION_KEY');
+    res.render('register', {
+        layout: false,
+        locals: {
+            name        : '',
+            captcha     : solvemedia.toHTML(solvemedia.LANG.FR, solvemedia.THEME.BLACK), 
+            errorMessage: ''
+                
+        }
+    });
+});
+
+
+app.get('/black', function(req, res) {
+
+    var solvemedia = new Solvemedia('PUBLIC_KEY','PRIVATE_KEY', 'AUTHENTICATION_KEY');
+    res.render('register', {
+        layout: false,
+        locals: {
+            name        : '',
+            captcha     : solvemedia.toHTML(solvemedia.THEME.BLACK), 
+            errorMessage: ''
+                
+        }
+    });
+});
+
+app.get('/fr', function(req, res) {
+
+    var solvemedia = new Solvemedia('PUBLIC_KEY','PRIVATE_KEY', 'AUTHENTICATION_KEY');
+    res.render('register', {
+        layout: false,
+        locals: {
+            name        : '',
+            captcha     : solvemedia.toHTML(solvemedia.LANG.FR), 
+            errorMessage: ''
+                
+        }
+    });
+});
+
+app.post('/validate', function(req, res) {
+    var solvemedia = new Solvemedia('PUBLIC_KEY','PRIVATE_KEY', 'AUTHENTICATION_KEY');
+    
+    solvemedia.verify(req.body.adcopy_response,req.body.adcopy_challenge, req.connection.remoteAddress, function(isValid,errorMessage){
+        if (isValid) {
+            res.send('Hi ' + req.body.name + ', Solvemedia told me that you are not a robot!!');
+        } else {
+            // Redisplay the form.
+            res.render('register', {
+                layout: false,
+                locals: {
+                    name        : req.body.name,
+                    captcha     : solvemedia.toHTML(), 
+                    errorMessage: errorMessage
+                }
+            });                            
+        }
+    });
+});        
+
+//Launch the HTTP server
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});
